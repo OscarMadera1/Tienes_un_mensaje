9a1d1ad064372b40c7673d90819013087f345e74
OscarMadera1 <omaderanegrete@gmail.com>
2023-04-27 23:23:04 -0500

    Agregando archivo csv en convert_to_time.py

--- convert_to_time.py
+++ convert_to_time.py
@@ -1,11 +1,28 @@
-from read_csv import df 
-import pandas as pd
 import datetime
+import csv
 
 
+"""
 df['FechaNacimiento'] = pd.to_datetime(df['FechaNacimiento'],format="%Y/%m/%d")
 
 df['year'] = df['FechaNacimiento'].dt.year
 df['month'] = df['FechaNacimiento'].dt.month
-df['day'] = df['FechaNacimiento'].dt.day
+df['day'] = df['FechaNacimiento'].dt.day"""
 
+today = datetime.datetime.now()
+nombres =[]
+direcciones_correo = []
+with open(r'C:\Users\DARYS\Desktop\DigitalNao\Reto3\Tienes_un_mensaje\employee.csv', newline='') as archivo_csv:
+    lector_csv = csv.reader(archivo_csv)
+    encabezados = next(lector_csv) 
+
+    for fila in lector_csv:
+        nombre = fila
+        fecha_Nacimiento = datetime.datetime.strptime(fecha_Nacimiento, '%Y-%m-%d').date()
+
+        if fecha_Nacimiento.month == today.month and fecha_Nacimiento.day == today.day:
+            nombres.append(nombre)
+            direcciones_correo.append(correo)
+
+print(nombres)
+print(direcciones_correo)
